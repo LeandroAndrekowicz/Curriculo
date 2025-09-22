8254e82c37cc40d62d9eec047f5d739da0409376
Leandro Andrekowicz <103007824+LeandroAndrekowicz@users.noreply.github.com>
2025-09-22 20:29:00 -0300

    Reordenação do projeto e correção de dependencias defazadas

--- src/componentes/Projetos/index.jsx
+++ src/componentes/Projetos/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Projetos.css'
-import {MdOutlineQuiz, MdDomainVerification} from 'react-icons/md'
-import {GiGalaxy} from 'react-icons/gi'
+import { MdOutlineQuiz, MdDomainVerification } from 'react-icons/md'
+import { GiGalaxy } from 'react-icons/gi'
 
 const Projetos = () => {
   return (
@@ -13,41 +13,44 @@ const Projetos = () => {
         <div className='container-cards'>
           <div className='card'>
             <i>
-              <MdOutlineQuiz/>
+              <MdOutlineQuiz />
             </i>
             <h5>Quiz conhecimentos gerais</h5>
             <p>Quiz desenvolvido em ReactJS com intuito de aprimorar o conceito de SinglePage</p>
-            <div>
+            <div className='links-projeto'>
               <a href="https://quiz-puce-six.vercel.app/">Vizualizar</a>
+              <a href="https://github.com/LeandroAndrekowicz/Quiz">Github</a>
             </div>
           </div>
         </div>
         <div className='container-cards'>
           <div className='card'>
             <i>
-              <GiGalaxy/>
+              <GiGalaxy />
             </i>
             <h5>Gerador de fotos Aleatórias</h5>
             <p>Gerador de fotos desenvolvido em ReactJS com o intuito de aprimorar o consumo de API's</p>
-            <div>
+            <div className='links-projeto'>
               <a href="https://fotos-marte.vercel.app/">Vizualizar</a>
+              <a href="https://github.com/LeandroAndrekowicz/Nasa">Github</a>
             </div>
           </div>
         </div>
         <div className='container-cards'>
           <div className='card'>
             <i>
-              <MdDomainVerification/>
+              <MdDomainVerification />
             </i>
-            <h5>Criador de Cards</h5>
-            <p>Criador de Cards com o intuito de aprimorar o conceito Reativo do ReactJS</p>
-            <div>
-              <a href="https://react-organo-six.vercel.app/">Vizualizar</a>
+            <h5>Classificador de e-mails</h5>
+            <p>Um classificador de e-mails utilizando OpenAI e Gemini para classificar e-mails como Produtivo ou Improdutivo e sugerir uma resposta</p>
+            <div className='links-projeto'>
+              <a href="https://desafio-autou-fu3i.onrender.com/">Vizualizar</a>
+              <a href="https://github.com/LeandroAndrekowicz/desafio-autoU">Github</a>
             </div>
           </div>
         </div>
       </div>
-      </div>
+    </div>
   )
 }
 
